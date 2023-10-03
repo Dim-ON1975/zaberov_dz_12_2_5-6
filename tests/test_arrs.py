430558166ee5fcc3e074f00bdf72bb260cf0bb48
Dim-ON1975 <zaberov.dv@internet.ru>
2023-10-03 22:57:35 +0300

    Добавлены новые тесты в функции модуля test_arrs.py, устраняющие ошибки покрытия кода тестами. В целях демонстрации результата работы (покрытие кода тестами) в репозиторий загружена папка htmlcov, содержащая анализ покрытия тестирования.

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -17,3 +17,6 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3, 4, 5], -1) == [5]
     assert arrs.my_slice([1, 2, 3, 4, 5], -5) == [1, 2, 3, 4, 5]
     assert arrs.my_slice([1, 2, 3, 4, 5], -3) == [3, 4, 5]
+    # Было пропущено тестирование строк 31 и 34. Добавляем тесты
+    assert arrs.my_slice([], 0) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5], None, 4) == [1, 2, 3, 4]
