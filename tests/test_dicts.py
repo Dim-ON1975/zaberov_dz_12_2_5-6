87a40c76f543e1dc9ac1c67ca30c9d3e4c4d342a
Dim-ON1975 <zaberov.dv@internet.ru>
2023-10-04 18:40:08 +0300

    1) Улучшены тесты в файле test_dicts.py посредством параметризации, позволяющей определить множество аргументов и фикстур для тестовой функции. 2) Добавлен html-отчёт, демонстрирующий 100% покрытие кода тестами.

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,18 +1,23 @@
+import pytest
+from typing import Union
 from utils.dicts import get_val
 
 data1 = {'vcs': 'mercurial', 1: 2023}
 data2 = {}
 
 
-def test_get_val():
-    assert get_val(data1, 'vcs') == 'mercurial'
-    assert get_val(data1, 1) == 2023
-    assert get_val(data1, 'vcs', 'git') == 'mercurial'
-    assert get_val(data1, 'vc', 'git') == 'git'
-    assert get_val(data1, 1, 'git') == 2023
-    assert get_val(data1, 0, 'git') == 'git'
-    assert get_val(data2, 'vcs', 'git') == 'git'
-    assert get_val(data2, 1, 'git') == 'git'
-    assert get_val(data2, 'vcs', 'bazaar') == 'bazaar'
-    assert get_val(data2, 1, 'bazaar') == 'bazaar'
-    assert get_val(data2, 0, 'bazaar') == 'bazaar'
+@pytest.mark.parametrize('dict_col, key, def_col, expected', [
+    (data1, 'vcs', '', 'mercurial'),
+    (data1, 1, '', 2023),
+    (data1, 'vcs', 'git', 'mercurial'),
+    (data1, 'vc', 'git', 'git'),
+    (data1, 1, 'git', 2023),
+    (data1, 0, 'git', 'git'),
+    (data2, 'vcs', 'git', 'git'),
+    (data2, 1, 'git', 'git'),
+    (data2, 'vcs', 'bazaar', 'bazaar'),
+    (data2, 1, 'bazaar', 'bazaar'),
+    (data2, 0, 'bazaar', 'bazaar')
+])
+def test_get_val(dict_col: dict, key: Union[str, int], def_col: str, expected: str):
+    assert get_val(dict_col, key, def_col) == expected
